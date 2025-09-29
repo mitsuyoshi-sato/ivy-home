3bcb8ffed31a389658906183ba4a4d7957ee0da7
mitsuyoshi-sato <mitsuyoshi.sato4256@gmail.com>
2025-09-30 00:04:05 +0900

    ivy-homeのロゴを設置

--- app/Header/index.tsx
+++ app/Header/index.tsx
@@ -6,14 +6,11 @@ export const Header = () => {
     <header className="fixed left-0 right-0 top-[20px] z-50 w-[95%] mx-auto">
       <nav className="px-4 py-2 fixed left-1/2 top-0 z-50 mt-4 w-[95%] -translate-x-1/2 rounded-full border border-white/30 bg-white/40 shadow-lg shadow-white/20 backdrop-blur-md backdrop-saturate-150">
         <div className="flex items-center justify-between">
-          <div className="flex items-center text-xl font-bold">
-            <Leaf
-              size={24}
-              className="-translate-y-[10px] translate-x-[10px] text-green-800"
-              fill="#86EFAC"
-            />
-            <div>Ivy Home</div>
-          </div>
+          <img
+            src="/ivy-logo.svg"
+            alt="logo"
+            className="w-[100px] -translate-y-[5px]"
+          />
           <Nav />
         </div>
       </nav>
