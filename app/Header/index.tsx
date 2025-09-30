b2a33d9a209773217e2bcf488771a7f097ffce55
mitsuyoshi-sato <mitsuyoshi.sato4256@gmail.com>
2025-09-30 10:38:25 +0900

    ロゴ修正

--- app/Header/index.tsx
+++ app/Header/index.tsx
@@ -5,11 +5,11 @@ export const Header = () => {
   return (
     <header className="fixed left-0 right-0 top-[20px] z-50 w-[95%] mx-auto">
       <nav className="px-4 py-2 fixed left-1/2 top-0 z-50 mt-4 w-[95%] -translate-x-1/2 rounded-full border border-white/30 bg-white/40 shadow-lg shadow-white/20 backdrop-blur-md backdrop-saturate-150">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between pl-2">
           <img
-            src="/ivy-logo.svg"
+            src="/ivy-home.svg"
             alt="logo"
-            className="w-[100px] -translate-y-[5px]"
+            className="w-[120px] -translate-y-[5px]"
           />
           <Nav />
         </div>
