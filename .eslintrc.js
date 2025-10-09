9080dd18c6489f9dc3c7358a304dcf43125c5b63
mitsuyoshi-sato <mitsuyoshi.sato4256@gmail.com>
2025-10-09 17:17:49 +0900

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: '@soichiro_nitta/eslint-config/presets/react-typescript-prettier',
+  plugins: ['sort-keys-fix'],
+  rules: {
+    'sort-keys-fix/sort-keys-fix': 'warn',
+    'typescript-sort-keys/interface': 'warn',
+    'react/jsx-sort-props': 'warn',
+  },
 }
