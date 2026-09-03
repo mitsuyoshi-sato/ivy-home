1c40f8a7a000ac1e9a688677891f70667686fa38
mitsuyoshi-sato <mitsuyoshi.sato4256@gmail.com>
2026-09-03 16:48:30 +0900

    feat(lp): 太陽光・蓄電池LPを構成

---
+++ app/(lp)/solar-battery/page.tsx
@@ -0,0 +1,25 @@
+import { _Benefits } from './_Benefits'
+import { _Cta } from './_Cta'
+import { _EnergySteps } from './_EnergySteps'
+import { _Faq } from './_Faq'
+import { _Hero } from './_Hero'
+import { _InstallationFlow } from './_InstallationFlow'
+import { _Reasons } from './_Reasons'
+import { _Works } from './_Works'
+
+const Page = () => {
+  return (
+    <>
+      <_Hero />
+      <_Benefits />
+      <_EnergySteps />
+      <_Works />
+      <_Reasons />
+      <_InstallationFlow />
+      <_Faq />
+      <_Cta />
+    </>
+  )
+}
+
+export default Page
